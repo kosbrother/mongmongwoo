9f99f200be3945a6d0c842cb2fd60c0c6229de10
varbark <naoki3355@gmail.com>
2016-04-18 20:36:51 +0800

    fix application.yml symlink

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ set :server_name, 'localhost'
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
 
-server '104.155.231.222', user: 'deploy', roles: %w{web app db}, primary: true
+server '104.155.220.215', user: 'deploy', roles: %w{web app db}, primary: true
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
 set :rails_env, :production
