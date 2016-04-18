5eea9f9bbf5ec90f556bf66b34bc6a427bdca0ab
gvszg <zetaentranceserious@gmail.com>
2016-04-18 21:06:02 +0800

    Add application.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :keep_releases, 5
 
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/storage}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/storage config/application.yml}
 
 # what specs should be run before deployment is allowed to
 # continue, see lib/capistrano/tasks/run_tests.cap
