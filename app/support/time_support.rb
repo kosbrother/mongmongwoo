163b61d82787462bccadc7b7a4fa1c39e29a77f7
gvszg <zetaentranceserious@gmail.com>
2016-05-02 12:23:59 +0800

    Modified code indent

--- app/support/time_support.rb
+++ app/support/time_support.rb
@@ -2,10 +2,10 @@ class TimeSupport
   attr_reader :time_param
 
   TIME_RANGE = {
-                  "month" => Range.new((Time.now - 30.day), Time.now),
-                  "week" => Range.new((Time.now - 7.day), Time.now),
-                  "day" => Range.new((Time.now - 1.day), Time.now)
-                }
+    "month" => Range.new((Time.now - 30.day), Time.now),
+    "week" => Range.new((Time.now - 7.day), Time.now),
+    "day" => Range.new((Time.now - 1.day), Time.now)
+  }
 
   def self.time_until(time_param)
     TIME_RANGE[time_param]
