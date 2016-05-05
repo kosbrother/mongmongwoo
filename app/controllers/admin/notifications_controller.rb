244cddcb367f22826389400bf24d98c095c23509
Steven Ko <chunyuko85@gmail.com>
2016-05-05 18:45:24 +0800

    fix notification problem

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -19,20 +19,23 @@ class Admin::NotificationsController < AdminController
 
     if @notification.save!
       gcm = GCM.new("AIzaSyAUjlCMS-ENLXfqGkSaOLDIZtz5BihP0kM")
-      registration_ids = DeviceRegistration.select(:registration_id).map(&:registration_id)
-      options = {
-        data: {
-          content_title: @notification.content_title,
-          content_text: @notification.content_text,
-          content_pic: @notification.send_content_pic,
-          item_id: @notification.item_id,
-          item_name: @notification.item.name,
-          item_price: @notification.item.price
-          },
-        collapse_key: "updated_score"
-      }
-      response = gcm.send_notification(registration_ids, options)
-
+      
+      DeviceRegistration.select(:id, :registration_id).find_in_batches do |ids|
+        registration_ids = ids.map(&:registration_id)
+        options = {
+          data: {
+            content_title: @notification.content_title,
+            content_text: @notification.content_text,
+            content_pic: @notification.send_content_pic,
+            item_id: @notification.item_id,
+            item_name: @notification.item.name,
+            item_price: @notification.item.price
+            },
+          collapse_key: "updated_score"
+        }
+        response = gcm.send_notification(registration_ids, options)
+      end
+      
       flash[:notice] = "成功推播訊息"
       redirect_to admin_notifications_path
     else
