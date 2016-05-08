6174a2ce9f5462a3c3753a9846132827b8419372
gvszg <zetaentranceserious@gmail.com>
2016-05-08 21:43:13 +0800

    User and device relation

--- app/models/device_registration.rb
+++ app/models/device_registration.rb
@@ -1,4 +1,6 @@
 class DeviceRegistration < ActiveRecord::Base
   validates_presence_of :registration_id, on: :create, message: "裝置ID不得空白"
   validates_uniqueness_of :registration_id, on: :create, message: "裝置ID重複了"
+
+  belongs_to :user
 end
