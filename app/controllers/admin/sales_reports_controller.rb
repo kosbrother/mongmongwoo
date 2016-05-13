a3497a155657496b095820fc35ca03998bde83fc
gvszg <zetaentranceserious@gmail.com>
2016-05-13 15:52:28 +0800

    Fix data nil and divided by 0

--- app/controllers/admin/sales_reports_controller.rb
+++ app/controllers/admin/sales_reports_controller.rb
@@ -42,11 +42,11 @@ class Admin::SalesReportsController < AdminController
       # f. 毛利(b - c - d - e)
       @gross_profit = (@total_sales_income - @total_cost_of_goods - @total_cost_of_freight_in - @total_cost_ship_fee)
       # g. 平均訂單毛利(f / a)
-      @gross_profit_per_order = @gross_profit / @total_order_quantity
+      @gross_profit_per_order = get_division_operator_value(@gross_profit, @total_order_quantity)
       # h. 總廣告費用
       @total_cost_of_advertising = @total_cost.total_cost_of_advertising
       # i. 平均廣告費用(h / a)
-      @average_cost_of_advertising = @total_cost_of_advertising / @total_order_quantity
+      @average_cost_of_advertising = get_division_operator_value(@total_cost_of_advertising, @total_order_quantity)
       # j. 平均扣廣告訂單毛利(g - i)
       @average_gross_profit_except_ads = @gross_profit_per_order - @average_cost_of_advertising
       # k. 取消訂單數
@@ -58,7 +58,7 @@ class Admin::SalesReportsController < AdminController
       # n. 實際毛利(f - l)
       @actual_gross_profit = @gross_profit - @cancelled_order_amount
       # o. 實際平均訂單毛利(f - l / a - k)
-      @actual_average_gross_profit = @actual_gross_profit / (@total_order_quantity - @cancelled_order_quantity)
+      @actual_average_gross_profit = get_division_operator_value(@actual_gross_profit, (@total_order_quantity - @cancelled_order_quantity))
     end
   end
 
@@ -73,12 +73,18 @@ class Admin::SalesReportsController < AdminController
   end
 
   def find_order_and_cost_data
-    @total_cost = CostStatistic.cost_date_within(search_date_params).select("SUM(cost_of_goods) AS total_cost_of_goods", "SUM(cost_of_freight_in) AS total_cost_of_freight_in", "SUM(cost_of_advertising) AS total_cost_of_advertising")[0]
-    @total_order = Order.created_at_within(search_date_params).select("COUNT(*) AS total_order_quantity", "SUM(ship_fee) AS total_cost_ship_fee")[0]
-    @cancelled_order = Order.cancelled_at_within(search_date_params).select("COUNT(*) AS cancelled_order_quantity", "SUM(items_price) AS cancelled_order_amount", "SUM(ship_fee) AS cancelled_order_ship_fee")[0]
+    @total_cost = CostStatistic.cost_date_within(search_date_params).select("COALESCE(SUM(cost_of_goods), 0) AS total_cost_of_goods", "COALESCE(SUM(cost_of_freight_in), 0) AS total_cost_of_freight_in", "COALESCE(SUM(cost_of_advertising), 0) AS total_cost_of_advertising")[0]
+    @total_order = Order.created_at_within(search_date_params).select("COUNT(*) AS total_order_quantity", "COALESCE(SUM(ship_fee), 0) AS total_cost_ship_fee")[0]
+    @cancelled_order = Order.cancelled_at_within(search_date_params).select("COUNT(*) AS cancelled_order_quantity", "COALESCE(SUM(items_price), 0) AS cancelled_order_amount", "COALESCE(SUM(ship_fee), 0) AS cancelled_order_ship_fee")[0]
   end
 
   def search_date_params
     params[:start_cost_date].to_date..params[:end_cost_date].to_date if params[:start_cost_date] && params[:end_cost_date]
+  end
+
+  def get_division_operator_value(value_1, value_2)
+    value_1 / value_2
+    rescue ZeroDivisionError
+    0
   end
 end
