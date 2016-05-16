45b64fb5ba6b0ea55e4a3e28332d2a98f635bdb8
gvszg <zetaentranceserious@gmail.com>
2016-05-16 19:24:16 +0800

    Add taobao supplier to salse report page

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,6 @@
 class OrderItem < ActiveRecord::Base
-  scope :sort_by_sales, -> { includes(:item, item: :categories).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity) as sum_item_quantity").order("sum_item_quantity DESC") }
-  scope :sort_by_revenue, -> { includes(:item, item: :categories).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity * item_price) as sum_item_revenue").order("sum_item_revenue DESC") }
+  scope :sort_by_sales, -> { includes(:item, item: [:categories, :taobao_supplier]).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity) as sum_item_quantity").order("sum_item_quantity DESC") }
+  scope :sort_by_revenue, -> { includes(:item, item: [:categories, :taobao_supplier]).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity * item_price) as sum_item_revenue").order("sum_item_revenue DESC") }
   scope :created_at_within, -> (time_param) { where(created_at: time_param) }
   scope :product_sales_created_at, -> (item_id, time_param) { select(:id, :item_name, :source_item_id, "SUM(item_quantity) as sum_item_quantity").where(source_item_id: item_id, created_at: time_param) }
   scope :product_sales, -> (item_id){ select("SUM(item_quantity) as sum_item_quantity").where(source_item_id: item_id) }
@@ -11,7 +11,11 @@ class OrderItem < ActiveRecord::Base
   belongs_to :item, :foreign_key => "source_item_id"
   belongs_to :item_spec
 
-  delegate :categories, to: :item
+  delegate :categories, :taobao_supplier, to: :item
 
   validates_presence_of :source_item_id, allow_blank: true
+
+  def get_taobao_supplier_name
+    item.supplier_name rescue "沒有商家資料"
+  end
 end
