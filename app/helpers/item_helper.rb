90584d1cb5869f67611b29cab3533de99b24e37d
varbark <naoki3355@gmail.com>
2016-05-24 21:23:16 +0800

    use i18n for translate item status

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -2,13 +2,4 @@ module ItemHelper
   def render_favorite_btn(status)
     link_to '加入收藏', '', id: 'add-favorite' , class: "add -wishlist #{ status ? "checked" : "uncheck"} ", remote: true, 'data-id': @item.id
   end
-
-  def transfer_item_status(status)
-    case status
-      when 'on_shelf'
-        '已上架'
-      when 'off_shelf'
-        '已下架'
-    end
-  end
 end
