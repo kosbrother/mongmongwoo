8e769ea593b2119886e057a4adbefd50ce7ffb1b
varbark <naoki3355@gmail.com>
2016-05-25 10:33:46 +0800

    require_user in orders cotroller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,14 @@
 class OrdersController < ApplicationController
   layout 'user'
 
-  before_action  :load_categories
+  before_action  :load_categories, :require_user
 
   def index
     @orders = current_user.orders
   end
 
   def show
-    @order = Order.find(params[:id])
+    @order = current_user.orders.find(params[:id])
     @items = @order.items.includes(:item_spec)
     @info = @order.info
   end
