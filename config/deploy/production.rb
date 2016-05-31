1b46b9dd2d91597555a7fcf0cac98eed7e2de5ae
varbark <naoki3355@gmail.com>
2016-05-31 10:26:03 +0800

    remove db from role

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
 
 server '104.155.220.215', user: 'deploy', roles: %w{web app db}, primary: true
-server 'www.mmwooo.com', user: 'deploy', roles: %w{web app db}, primary: true
+server 'www.mmwooo.com', user: 'deploy', roles: %w{web app}, primary: true
 
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
