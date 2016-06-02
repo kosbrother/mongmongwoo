0a93a1000d2d657183a724f030165899b11306ad
gvszg <zetaentranceserious@gmail.com>
2016-06-02 14:07:44 +0800

    Modified town api spec

--- spec/controller/api/v3/towns_controller_spec.rb
+++ spec/controller/api/v3/towns_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V3::TownsController, type: :controller do
-  let!(:county) { FactoryGirl.create(:county) }
+  let!(:county) { FactoryGirl.create(:seven_store_county) }
   let!(:town) { FactoryGirl.create(:town, county: county) }
 
   describe "Get index" do
@@ -12,11 +12,13 @@ RSpec.describe Api::V3::TownsController, type: :controller do
     it_behaves_like "return correct response format" do
       let(:action) { get :index, county_id: county.id }
     end
-
-    it "should return correct town data" do
+    
+    it "should find county's towns" do
       get :index, county_id: county.id
       json = JSON.parse(response.body)
-      expect(json[0]['name']).to eq(town.name)
+      expect(json.length).to eq(county.towns.length)
+      expect(json[0]['id']).to eq(county.towns[0].id)
+      expect(json[0]['name']).to eq(county.towns[0].name)
     end
   end
 end
