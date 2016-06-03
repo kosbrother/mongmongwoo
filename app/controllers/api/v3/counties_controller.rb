d8c51d92ccb9fdc53f3557ece71c292938be9f1b
gvszg <zetaentranceserious@gmail.com>
2016-06-03 13:53:45 +0800

    Refactor county query

--- app/controllers/api/v3/counties_controller.rb
+++ app/controllers/api/v3/counties_controller.rb
@@ -1,6 +1,6 @@
 class Api::V3::CountiesController < ApiController
   def index
-    counties = County.seven_stores.select(:id, :name)
+    counties = County.seven_stores.id_and_name
 
     render json: counties
   end
--- app/models/county.rb
+++ app/models/county.rb
@@ -1,5 +1,6 @@
 class County < ActiveRecord::Base
   scope :seven_stores, -> { where("store_type = ?", "4") }
+  scope :id_and_name, -> { select(:id, :name) }
 
   has_many :towns
   has_many :stores
