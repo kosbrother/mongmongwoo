deb539a2a06dabc6003baca9f15ef9732712ec73
gvszg <zetaentranceserious@gmail.com>
2016-06-03 22:27:37 +0800

    Refactor device registration v3 api

--- app/controllers/api/v3/device_registrations_controller.rb
+++ app/controllers/api/v3/device_registrations_controller.rb
@@ -1,7 +1,9 @@
 class Api::V3::DeviceRegistrationsController < ApiController
   def create
     if DeviceRegistration.find_or_create_by!(registration_id: params[:registration_id])
-      render json: "Success：新增裝置完成"
+      render status: 200, json: "Success"
+    else
+      render status: 400, json: "Fail"
     end
   end
 end
