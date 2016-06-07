0879bff3051c9ff573e310f37aeb6fefc0cc6b35
Steven Ko <chunyuko85@gmail.com>
2016-06-07 12:03:26 +0800

    error handle api exception

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,8 @@
 class ApiController < ActionController::Base
+  rescue_from Exception do |e|
+    error(e)
+  end
+
   def android_version
     render json: AndroidVersion.last
   end
@@ -17,4 +21,16 @@ class ApiController < ActionController::Base
     @road = @town.roads.find(params[:road_id])
   end
 
+  def error(e)
+    if env["ORIGINAL_FULLPATH"] =~ /^\/api/
+      error_info = {
+        :exception => "#{e.class.name} : #{e.message}",
+      }
+      error_info[:trace] = e.backtrace[0,10] if Rails.env.development?
+      Rails.logger.error("error: #{error_info}")
+      render status: 500, json: {error: {message: error_info}}
+    else
+      raise e
+    end
+  end
 end
