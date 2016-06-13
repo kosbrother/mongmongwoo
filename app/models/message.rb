7363a4c9edafc56e612473da467392ae15361f1f
gvszg <zetaentranceserious@gmail.com>
2016-06-13 10:32:57 +0800

    Message order by created_at

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  scope :official_messages, -> { where(message_type: Message.message_types["萌萌屋官方訊息"]).order(id: :DESC) }
+  scope :official_messages, -> { where(message_type: Message.message_types["萌萌屋官方訊息"]).order(created_at: :DESC) }
   scope :select_api_fields, -> { select(:id, :message_type, :title, :content, :created_at) }
 
   enum message_type: { "萌萌屋官方訊息": "0", "個人訊息": "1" }
