5fc1f9ab411a1ebfbce8e74dda737654809ee119
varbark <naoki3355@gmail.com>
2016-06-15 10:40:44 +0800

    add spec for multiple oauth logins

--- spec/controller/api/v3/oauth_sessions_controller_spec.rb
+++ spec/controller/api/v3/oauth_sessions_controller_spec.rb
@@ -23,8 +23,8 @@ RSpec.describe Api::V3::OauthSessionsController, type: :controller do
 
 
     context 'when user and login exist' do
-      user = FactoryGirl.create(:user)
-      login = FactoryGirl.create(:login, :facebook, user_id: user.id)
+      let!(:user) { FactoryGirl.create(:user) }
+      let!(:login) { FactoryGirl.create(:login, :facebook, user_id: user.id) }
       let!(:uid) { login.uid }
       let!(:user_name) { login.user_name }
       let!(:email) { user.email }
@@ -32,6 +32,16 @@ RSpec.describe Api::V3::OauthSessionsController, type: :controller do
       it 'does not create new user and new fb login' do
         expect(User.where(email: email).size).to eq(1)
         expect(Login.where(provider: provider, uid: uid).size).to eq(1)
+      end
+
+
+      context 'when login from different provider with same email' do
+        let!(:provider) { 'google' }
+        it 'does belong to same user and create new provider login' do
+          another_login = Login.find_by(provider: provider, uid: uid)
+          expect(User.where(email: email).size).to eq(1)
+          expect(another_login).to be_present
+        end
       end
     end
   end
