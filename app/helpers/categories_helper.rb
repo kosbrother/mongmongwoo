18b0a2dcdc09f0c4623e3381bb9cb41bf4cd45fb
gvszg <zetaentranceserious@gmail.com>
2016-06-15 23:00:07 +0800

    Modified image helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -6,14 +6,4 @@ module CategoriesHelper
       'btn btn-default'
     end
   end
-  
-  def category_photo(category)
-    if category.image_url.present?
-      image_url = category.image_url
-    else
-      image_url = "http://placehold.it/150x150&text=No Pic"
-    end
-
-    image_tag(image_url, size: "120x120")
-  end
 end
