874335ebefe4520ac810261742185c1fb69471b1
Steven Ko <chunyuko85@gmail.com>
2016-06-16 14:17:51 +0800

    item api send 'slug'

--- app/controllers/api/v3/items_controller.rb
+++ app/controllers/api/v3/items_controller.rb
@@ -1,14 +1,14 @@
 class Api::V3::ItemsController < ApiController
   def index
     category = Category.find(params[:category_id])
-    items = category.items.on_shelf.priority.includes(:specs).select(:id, :name, :price, :special_price, :cover).page(params[:page]).per_page(20)
+    items = category.items.on_shelf.priority.includes(:specs).select(:id, :name, :price, :special_price, :cover, :slug).page(params[:page]).per_page(20)
 
     render status: 200, json: {data: items.as_json(include: { specs: { only: [:id, :style, :style_pic] } })}
   end
 
   def show
     category = Category.find(params[:category_id])
-    item = category.items.select(:id, :name, :price, :special_price, :cover, :description, :status).find(params[:id])
+    item = category.items.select(:id, :name, :price, :special_price, :cover, :description, :status, :slug).find(params[:id])
     item_json = item.as_json
     specs = item.specs.on_shelf.select(:id,:style,:style_pic)
     photos = item.photos.select(:id, :image)
