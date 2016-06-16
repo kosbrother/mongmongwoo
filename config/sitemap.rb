301e10c64b9d9894e81efbff68fda6f3418c3916
Steven Ko <chunyuko85@gmail.com>
2016-06-16 14:14:41 +0800

    fix meta&sitemap android scheme

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,14 +3,14 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'https://www.mmwooo.com/'
 SitemapGenerator::Sitemap.create do
-  add '/', :changefreq => 'daily', :priority => 0.9
+  add '/', :changefreq => 'daily', :priority => 0.9, alternate: { href: "android-app://com.kosbrother.mongmongwoo/https/www.mmwooo.com", lang: "zh-TW"}
   Category.find_each do |category|
     add category_path(category),  :changefreq => 'daily', :priority => 0.9
   end
   Item.find_each do |item|
     item.categories.each do |category|
       add category_item_path(category, item),  :changefreq => 'daily', :priority => 0.9 ,
-          alternate: { href: "android-app://com.kosbrother.mongmongwoo/mmwooo/www.mmwooo.com/categories/#{category.slug}-#{category.id}/items/#{item.slug}-#{item.id}",
+          alternate: { href: "android-app://com.kosbrother.mongmongwoo/https/www.mmwooo.com/categories#{category_item_path(category,item)}",
                         lang: "zh-TW"}
     end
   end
