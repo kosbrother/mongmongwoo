c8e1c1abe46d75469ed07ca4ff4121bc01b22636
varbark <naoki3355@gmail.com>
2016-06-17 12:10:15 +0800

    web:  password reset page and controller action

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,6 @@ class User < ActiveRecord::Base
     self.password_reset_token = SecureRandom.urlsafe_base64
     self.password_reset_sent_at = Time.zone.now
     self.save
-    UserMailer.password_reset(self).deliver
+    UserMailer.delay.password_reset(self)
   end
 end
