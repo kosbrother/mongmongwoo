4e20762e7d6b3d66c82bc066b57d594dda4b2de3
gvszg <zetaentranceserious@gmail.com>
2016-06-20 15:38:02 +0800

    Modify blasklist format regex

--- app/models/order_blacklist.rb
+++ app/models/order_blacklist.rb
@@ -1,6 +1,6 @@
 class OrderBlacklist < ActiveRecord::Base
-  OrderBlacklist::Email_Format = /\A[a-z0-9._%+]+@[a-z0-9.]+\.[a-z]{2,4}\Z/i
-  OrderBlacklist::Phone_Format = /\A(0)(9)+\d{8}\Z/
+  OrderBlacklist::Email_Format = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  OrderBlacklist::Phone_Format = /\A(\+\d{3}-)?(\(?(0)?([2-9])\)?)(-)?(\d{2})?(-)?\d{3,4}(-)?\d{3,4}\z/
 
   scope :email_blacklists, -> { uniq.map(&:email) }
   scope :phone_blacklists, -> { uniq.map(&:phone) }
