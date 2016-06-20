4a6730173cc4f9b05b5b34b1d82ab90d98c0af75
varbark <naoki3355@gmail.com>
2016-06-20 21:34:23 +0800

    admin can update item spec id

--- app/controllers/admin/admin_cart_items_controller.rb
+++ app/controllers/admin/admin_cart_items_controller.rb
@@ -1,11 +1,13 @@
 class Admin::AdminCartItemsController < AdminController
+  before_action :find_cart_item, only: [:update_spec, :destroy]
 
   def create
     @cart_item = AdminCartItem.create(cart_item_params)
   end
 
   def update_spec
-
+    @cart_item.item_spec_id = params[:spec_item_id]
+    @cart_item.save
   end
 
   def update_quantity
@@ -13,11 +15,13 @@ class Admin::AdminCartItemsController < AdminController
   end
 
   def destroy
-    @cart_item = current_admin_cart.admin_cart_items.find(params[:id])
     @cart_item.destroy
   end
 
   private
+  def find_cart_item
+    @cart_item = current_admin_cart.admin_cart_items.find(params[:id])
+  end
 
   def cart_item_params
     params.permit(:item_id, :item_spec_id, :item_quantity).merge({ admin_cart_id: current_admin_cart.id})
