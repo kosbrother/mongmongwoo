c12c4f1d4195ffcac6338a85c56b2061bf7122b4
varbark <naoki3355@gmail.com>
2016-06-21 11:10:13 +0800

    preload first taobao supplier items and specs

--- app/controllers/admin/admin_carts_controller.rb
+++ app/controllers/admin/admin_carts_controller.rb
@@ -2,6 +2,9 @@ class Admin::AdminCartsController < AdminController
 
   def checkout
     @admin_cart_items = current_admin_cart.admin_cart_items.recent.includes({item: :specs}, :item_spec)
+    @taobao_list = TaobaoSupplier.all
+    @first_taobao_items = @taobao_list[0].items
+    @first_item_specs = @first_taobao_items[0].specs
   end
 
   def submit
