c170473f48e79bd625bff026ccc7ed9740c347c0
gvszg <zetaentranceserious@gmail.com>
2016-06-22 18:10:08 +0800

    Update order status if goods arrive store or pickup

--- app/controllers/allpay_controller.rb
+++ app/controllers/allpay_controller.rb
@@ -1,4 +1,6 @@
 class AllpayController < ActionController::Base
+  after_action :update_order_status_if_goods_arrive_store_or_pickup, only: [:create_reply]
+
   def create
     if PostToAllpayWorker.new.perform(params[:order_id], create_reply_allpay_index_url, status_update_allpay_index_url)
       @message = "已將編號：#{params[:order_id]} 成功傳送到歐付寶"
@@ -8,7 +10,18 @@ class AllpayController < ActionController::Base
   end
 
   def create_reply
-    order = Order.find(params[:MerchantTradeNo].to_i)
-    order.update_attributes(logistics_status_code: params[:RtnCode].to_i)
+    @order = Order.find(params[:MerchantTradeNo].to_i)
+    @order.update_attributes(logistics_status_code: params[:RtnCode].to_i)
+    render status: 200, json: { data: "success" }
+  end
+
+  private
+
+  def update_order_status_if_goods_arrive_store_or_pickup
+    if params[:RtnCode].to_i == 2073
+      @order.update_attributes(status: Order.statuses["已到店"])
+    elsif params[:RtnCode].to_i == 3022
+      @order.update_attributes(status: Order.statuses["完成取貨"])
+    end
   end
 end
