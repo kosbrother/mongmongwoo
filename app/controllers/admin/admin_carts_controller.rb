e618539e4dc53b105e3c3ed1477976aab2886de1
varbark <naoki3355@gmail.com>
2016-06-22 10:59:58 +0800

    Admin cart: cart should be group by different supplier id

--- app/controllers/admin/admin_carts_controller.rb
+++ app/controllers/admin/admin_carts_controller.rb
@@ -1,7 +1,6 @@
 class Admin::AdminCartsController < AdminController
 
   def checkout
-    @admin_cart_items = current_admin_cart.admin_cart_items.recent.includes({item: :specs}, :item_spec)
     @taobao_list = TaobaoSupplier.all
     @first_taobao_items = @taobao_list[0].items
     @first_item_specs = @first_taobao_items[0].specs
