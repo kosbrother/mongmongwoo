46aaad94cfef170487439cc8443b6217ca945032
varbark <naoki3355@gmail.com>
2016-06-24 15:27:08 +0800

    Admin cart: submit cart to shipping items

--- app/models/admin_cart.rb
+++ app/models/admin_cart.rb
@@ -1,4 +1,11 @@
 class AdminCart < ActiveRecord::Base
   has_many :admin_cart_items
+  has_many :shipping_items
   belongs_to :taobao_supplier
+
+  def transfer_to_shipping_items
+    self.admin_cart_items.select(:admin_cart_id, :item_id, :item_spec_id, :item_quantity ).each do |cart_item|
+      ShippingItem.create(cart_item.as_json)
+    end
+  end
 end
