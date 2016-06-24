c130db1558962b8df4cb2b89165698e37e9b5272
Steven Ko <chunyuko85@gmail.com>
2016-06-24 19:32:03 +0800

    use model select fields

--- app/controllers/api/v3/shop_infos_controller.rb
+++ app/controllers/api/v3/shop_infos_controller.rb
@@ -1,6 +1,6 @@
 class Api::V3::ShopInfosController < ApiController
   def index
-    shop_infos = ShopInfo.all.as_json({except: [:id, :created_at, :updated_at]})
+    shop_infos = ShopInfo.select(:id, :question, :answer).all
     render status: 200, json: shop_infos
   end
 end
