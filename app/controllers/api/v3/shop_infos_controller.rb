0d49614763889ab2a112c4e6b29e3e018238d9bc
varbark <naoki3355@gmail.com>
2016-06-24 18:28:05 +0800

    API v3 shop info: only return useful column

--- app/controllers/api/v3/shop_infos_controller.rb
+++ app/controllers/api/v3/shop_infos_controller.rb
@@ -1,7 +1,6 @@
 class Api::V3::ShopInfosController < ApiController
   def index
-    shop_infos = ShopInfo.all
-
-    render status: 200, json: {data: shop_infos}
+    shop_infos = ShopInfo.all.as_json({except: [:id, :created_at, :updated_at]})
+    render status: 200, json: shop_infos
   end
 end
