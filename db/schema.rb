4e5da92a1856c711ceaf9c954496e7071fb5f3c0
varbark <naoki3355@gmail.com>
2016-06-24 15:26:42 +0800

    create shipping_items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623030151) do
+ActiveRecord::Schema.define(version: 20160624031501) do
 
   create_table "admin_cart_items", force: :cascade do |t|
     t.integer  "admin_cart_id", limit: 4
@@ -383,6 +383,18 @@ ActiveRecord::Schema.define(version: 20160623030151) do
   end
 
   add_index "roads", ["store_type"], name: "index_roads_on_store_type", using: :btree
+
+  create_table "shipping_items", force: :cascade do |t|
+    t.integer "item_id",       limit: 4
+    t.integer "item_spec_id",  limit: 4
+    t.integer "admin_cart_id", limit: 4
+    t.integer "item_quantity", limit: 4
+    t.boolean "is_checked",              default: false
+  end
+
+  add_index "shipping_items", ["admin_cart_id"], name: "index_shipping_items_on_admin_cart_id", using: :btree
+  add_index "shipping_items", ["item_id"], name: "index_shipping_items_on_item_id", using: :btree
+  add_index "shipping_items", ["item_spec_id"], name: "index_shipping_items_on_item_spec_id", using: :btree
 
   create_table "shop_infos", force: :cascade do |t|
     t.string   "question",   limit: 255
