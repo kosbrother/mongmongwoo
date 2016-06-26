ef523a5f20459fd5a4a807aa360bfae88754383e
Steven Ko <chunyuko85@gmail.com>
2016-06-26 20:44:56 +0800

    api fix: shop_infos

--- app/controllers/api/v3/shop_infos_controller.rb
+++ app/controllers/api/v3/shop_infos_controller.rb
@@ -1,6 +1,6 @@
 class Api::V3::ShopInfosController < ApiController
   def index
     shop_infos = ShopInfo.select(:id, :question, :answer).all
-    render status: 200, json: shop_infos
+    render status: 200, json: {data: shop_infos}
   end
 end
--- spec/controller/api/v3/shop_infos_controller_spec.rb
+++ spec/controller/api/v3/shop_infos_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::V3::ShopInfosController, type: :controller do
   describe "get #index" do
     it 'does return all the shop infos' do
       get :index
-      data =  JSON.parse(response.body)
+      data = JSON.parse(response.body)["data"]
       expect(data.count).to eq(3)
       expect(response.status).to eq(200)
       expect(response.content_type).to eq("application/json")
