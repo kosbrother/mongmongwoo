de19918fc05bf201ea47cfc3cbd0801a195f6d76
gvszg <zetaentranceserious@gmail.com>
2016-06-27 14:15:21 +0800

    Redesign stocks and stock_specs tables

--- db/migrate/20160624055038_create_stock_specs.rb
+++ db/migrate/20160624055038_create_stock_specs.rb
@@ -2,12 +2,12 @@ class CreateStockSpecs < ActiveRecord::Migration
   def change
     create_table :stock_specs do |t|
       t.integer :stock_id
-      t.string :style
-      t.integer :style_amount
-      t.string :style_pic
+      t.integer :item_spec_id
+      t.integer :amount
       t.timestamps null: false
     end
 
     add_index :stock_specs, :stock_id
+    add_index :stock_specs, :item_spec_id
   end
 end
