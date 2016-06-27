f91f419447ee231616bdd09cac01c45736990ef3
varbark <naoki3355@gmail.com>
2016-06-27 21:26:08 +0800

    Refactor Admin carts and admin cart items controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -20,25 +20,20 @@ class AdminController < ActionController::Base
   end
 
   def current_admin_carts
-    session[:admin_cart_ids].blank? ? [] : AdminCart.where(id: session[:admin_cart_ids]).includes(:taobao_supplier, {admin_cart_items: [{item: :specs}, :item_spec]})
+    session[:admin_cart_ids] = [] unless session[:admin_cart_ids]
+    AdminCart.where(id: session[:admin_cart_ids]).includes(:taobao_supplier, {admin_cart_items: [{item: :specs}, :item_spec]})
   end
 
   def current_supplier_cart(supplier_id)
-    supplier_id = supplier_id.to_i
-    if current_admin_carts.any?
-      supplier_cart = current_admin_carts.select{|cart| cart.taobao_supplier_id == supplier_id}[0]
-      supplier_cart = create_supplier_cart(supplier_id) unless supplier_cart
-      supplier_cart
-    else
-      create_supplier_cart(supplier_id)
-    end
+    supplier_cart = current_admin_carts.find_by(taobao_supplier_id: supplier_id)
+    supplier_cart = create_supplier_cart(supplier_id) unless supplier_cart
+    supplier_cart
   end
 
   private
 
   def create_supplier_cart(supplier_id)
     admin_cart = AdminCart.create(taobao_supplier_id: supplier_id)
-    session[:admin_cart_ids] = [] unless session[:admin_cart_ids]
     session[:admin_cart_ids] << admin_cart.id
     admin_cart
   end
--- app/helpers/admin/admin_cart_helper.rb
+++ app/helpers/admin/admin_cart_helper.rb
@@ -1,6 +1,6 @@
 module Admin::AdminCartHelper
   def render_supplier_name(cart)
-    if cart.taobao_supplier_id > 0
+    if cart.taobao_supplier.present?
       cart.taobao_supplier.name
     else
       '未登記淘寶商家'
