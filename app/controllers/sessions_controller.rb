87b4e1af85e7301cfdf35f635be5c98dd2748c32
varbark <naoki3355@gmail.com>
2016-06-28 19:29:54 +0800

    User could login by Facebook or MMW email and password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,19 @@
 class SessionsController < ApplicationController
 
-  def create
+  def create_by_mmw
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      cart = current_cart
+      cart.user = user
+      cart.save
+    else
+      flash[:danger] = '帳號密碼錯誤'
+    end
+    redirect_to root_path
+  end
+
+  def create_by_auth
     auth = request.env["omniauth.auth"].extra.raw_info
     user = User.find_or_create_from_omniauth(auth)
     session[:user_id] = user.id
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
 
   root 'pages#index'
-  get  "/auth/:provider/callback" => "sessions#create", as: "login"
+
+  get  "/auth/:provider/callback" => "sessions#create_by_auth", as: "create_by_auth"
   get '/auth/:provider', to: lambda{|env| [404, {}, ["Not Found"]]}, as: 'auth'
+  post "/login" => "sessions#create_by_mmw", as: "create_by_mmw"
   get  "/signout" => "sessions#destroy", as: "signout"
+
+  post "/register" => "users#create", as: "create"
+  get "/forget" => "users#forget", as: "forget"
+  post "/forget" => "users#sent_reset_email"
 
   resources :categories, only: [:show] do
     resources :items, only: [:show]
