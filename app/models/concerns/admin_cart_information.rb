ec8b56768fd22fdfac2e925cacb925dd2e2c8aba
gvszg <zetaentranceserious@gmail.com>
2016-06-29 18:36:54 +0800

    Display shipping cart item quantity

---
+++ app/models/concerns/admin_cart_information.rb
@@ -0,0 +1,24 @@
+module AdminCartInformation
+  extend ActiveSupport::Concern
+
+  def shipping_item_quantity
+    shipping_item = get_shipping_item
+
+    if shipping_item
+      quantity = shipping_item.item_quantity
+      quantity
+    else
+      "無運送中商品"
+    end
+  end
+
+  private
+
+  def get_shipping_item
+    if self.class == ItemSpec
+      AdminCartItem.in_shipping_cart.find_by(item_spec_id: self.id)
+    elsif self.class == StockSpec
+      AdminCartItem.in_shipping_cart.find_by(item_spec_id: self.item_spec_id)
+    end    
+  end
+end
