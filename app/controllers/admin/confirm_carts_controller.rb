10e8aa506d8ece8daf734eb6771283c6b28f2974
gvszg <zetaentranceserious@gmail.com>
2016-07-01 18:00:14 +0800

    Refactor confirm action

--- app/controllers/admin/confirm_carts_controller.rb
+++ app/controllers/admin/confirm_carts_controller.rb
@@ -10,32 +10,41 @@ class Admin::ConfirmCartsController < AdminController
     shipping_cart = AdminCart.current_status(AdminCart::STATUS[:shipping]).find(params[:id])
     cart_items = shipping_cart.admin_cart_items
 
-    cart_items.each do |cart_item|
-      stock = Stock.find_by(item_id: cart_item.item_id)
-      stock_spec = StockSpec.find_by(item_spec_id: cart_item.item_spec_id)
-
-      if stock && stock_spec
-        stock_spec.amount += cart_item.item_quantity
-        stock_spec.save
-      elsif stock && stock_spec.nil?
-        new_stock_spec = stock.stock_specs.new
-        new_stock_spec.item_spec = cart_item.item_spec
-        new_stock_spec.amount = cart_item.item_quantity
-        new_stock_spec.save
-      else
-        new_stock = Stock.new
-        new_stock.item = cart_item.item
-        new_stock.save
-
-        new_stock_spec = new_stock.stock_specs.new
-        new_stock_spec.item_spec = cart_item.item_spec
-        new_stock_spec.amount = cart_item.item_quantity
-        new_stock_spec.save
-      end
-    end
+    set_cart_items_to_stock(cart_items)
 
     shipping_cart.update_attribute(:status, AdminCart::STATUS[:stock])
 
     redirect_to admin_confirm_carts_path(status: AdminCart::STATUS[:stock])
+  end
+
+  private
+
+  def set_cart_items_to_stock(cart_items)
+    cart_items.each do |cart_item|
+      save_stock_and_stock_spec(cart_item)
+    end
+  end
+
+  def save_stock_and_stock_spec(cart_item)
+    stock = Stock.find_or_initialize_by(item_id: cart_item.item_id)
+    stock_spec = StockSpec.find_or_initialize_by(item_spec_id: cart_item.item_spec_id)
+
+    if stock.new_record? && stock_spec.new_record?
+      stock.item = cart_item.item
+      stock.save
+
+      stock.stock_specs << stock_spec
+      stock_spec.item_spec = cart_item.item_spec
+      stock_spec.amount = cart_item.item_quantity
+      stock_spec.save
+    elsif stock_spec.new_record?
+      stock.stock_specs << stock_spec
+      stock_spec.item_spec = cart_item.item_spec
+      stock_spec.amount = cart_item.item_quantity
+      stock_spec.save
+    else
+      stock_spec.amount += cart_item.item_quantity
+      stock_spec.save
+    end
   end
 end
