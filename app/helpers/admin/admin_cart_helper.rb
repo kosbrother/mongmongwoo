6e4c68c5178c55602d1695f285927cb91df69d71
gvszg <zetaentranceserious@gmail.com>
2016-07-04 19:21:38 +0800

    Refactor taobao supplier name display

--- app/helpers/admin/admin_cart_helper.rb
+++ app/helpers/admin/admin_cart_helper.rb
@@ -1,12 +1,4 @@
 module Admin::AdminCartHelper
-  def render_supplier_name(cart)
-    if cart.taobao_supplier.present?
-      cart.taobao_supplier.name
-    else
-      '未登記淘寶商家'
-    end
-  end
-
   def li_cart_status_link(status)
     content_tag(:li, '' , class: set_class_to_active(status)) do
       link_to t(AdminCart::STATUS.key(status)), admin_confirm_carts_path(status: status)
