b19e504fe10ee2cadf02e0499faa0d3b7b4a3e0d
varbark <naoki3355@gmail.com>
2016-07-04 20:33:32 +0800

    web login: error handle for empty email and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,23 @@
 class UsersController < ApplicationController
   def create
     user = User.find_or_initialize_by(email: params[:email])
-    if user.password_digest
+    if params[:email].blank?
+      @message = '信箱不可為空白'
+      render 'register_error'
+    elsif params[:password].blank?
+      @message = '密碼不可為空白'
+      render 'register_error'
+    elsif user && user.password_digest
       @message = '信箱已被註冊，請重新輸入'
+      render 'register_error'
+    elsif user.invalid?
+      @message = '信箱格式錯誤，請重新輸入'
       render 'register_error'
     else
       user.password = params[:password]
-      if user.save
-        set_current_user_and_cart(user)
-        render 'partials/js/reload'
-      else
-        @message = '信箱格式錯誤，請重新輸入'
-        render 'register_error'
-      end
+      user.save
+      set_current_user_and_cart(user)
+      render 'partials/js/reload'
     end
   end
 
