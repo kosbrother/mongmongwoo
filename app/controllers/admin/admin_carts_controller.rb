499d614f5a0e8f0c4ac73b6d01622113cf726eb0
varbark <naoki3355@gmail.com>
2016-07-04 17:33:21 +0800

    admin cart: move method to admin_cart model

--- app/controllers/admin/admin_carts_controller.rb
+++ app/controllers/admin/admin_carts_controller.rb
@@ -1,5 +1,4 @@
 class Admin::AdminCartsController < AdminController
-  helper_method :supplier_name
 
   def checkout
     @taobao_list = TaobaoSupplier.all
@@ -9,23 +8,12 @@ class Admin::AdminCartsController < AdminController
 
   def submit
     admin_cart = AdminCart.find(params[:admin_cart_id])
-    if admin_cart.admin_cart_items.any?
-      admin_cart.update_attribute(:status, AdminCart::STATUS[:shipping])
+    if admin_cart.set_to_shipping
       session[:admin_cart_ids].delete(params[:admin_cart_id].to_i)
-      flash[:success] = "#{supplier_name(admin_cart)} 購物車已送出"
+      flash[:success] = "#{admin_cart.taobao_supplier_name} 購物車已送出"
     else
       flash[:danger] = '您尚未添加商品到此購物車，因此無法送出'
     end
     redirect_to :back
-  end
-
-  private
-
-  def supplier_name(cart)
-    if cart.taobao_supplier.present?
-      cart.taobao_supplier.name
-    else
-      '未登記淘寶商家'
-    end
   end
 end
--- app/models/admin_cart.rb
+++ app/models/admin_cart.rb
@@ -6,4 +6,18 @@ class AdminCart < ActiveRecord::Base
   enum status: {'cart': 0, 'shipping': 1, 'stock': 2}
 
   STATUS = { cart: 0, shipping: 1, stock: 2 }
+
+  def set_to_shipping
+    if self.admin_cart_items.any?
+      self.update_attribute(:status, AdminCart::STATUS[:shipping])
+    end
+  end
+
+  def taobao_supplier_name
+    if self.taobao_supplier.present?
+      self.taobao_supplier.name
+    else
+      '未登記淘寶商家'
+    end
+  end
 end
