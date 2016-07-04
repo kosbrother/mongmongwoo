5d74671f1c21e28d7befc8e7ecce9387ec44dfe9
varbark <naoki3355@gmail.com>
2016-07-04 10:50:58 +0800

    Move API MMW register error message to local zh-TW

--- app/controllers/api/v3/mmw_registrations_controller.rb
+++ app/controllers/api/v3/mmw_registrations_controller.rb
@@ -1,14 +1,14 @@
 class Api::V3::MmwRegistrationsController < ApiController
   def create
     if User.where.not(password_digest: nil).find_by(email: params[:email])
-      render  status: 400, json: {error: {message: 'email is already taken'}}
+      render  status: 400, json: {error: {message: t('controller.error.message.email_taken') }}
     else
       user = User.find_or_initialize_by(email: params[:email])
       user.password = params[:password]
       if user.save
         render status: 200, json: {data: "success"}
       else
-        render status: 400, json: {error: {message: 'email or password is not correct'}}
+        render status: 400, json: {error: {message: t('controller.error.message.wrong_email_format') }}
       end
     end
   end
@@ -16,11 +16,11 @@ class Api::V3::MmwRegistrationsController < ApiController
   def login
     user = User.find_by(email: params[:email])
     if user.nil? || user.password_digest.nil?
-      render status: 400, json: {error: {message: 'can not find user'}}
+      render status: 400, json: {error: {message: t('controller.error.message.no_user')}}
     elsif  user.authenticate(params[:password])
       render status: 200, json: {data: "success"}
     else
-      render status: 400, json: {error: {message: 'password is not correct'}}
+      render status: 400, json: {error: {message: t('controller.error.message.wrong_password')}}
     end
   end
 
@@ -30,7 +30,7 @@ class Api::V3::MmwRegistrationsController < ApiController
       user.sent_password_reset
       render status: 200, json: {data: "success"}
     else
-      render status: 400, json: {error: {message: 'can not find user'}}
+      render status: 400, json: {error: {message:  t('controller.error.message.no_user')}}
     end
   end
 end
