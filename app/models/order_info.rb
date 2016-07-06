ac910765a793c189687d575f3588c100b65d238f
gvszg <zetaentranceserious@gmail.com>
2016-07-06 22:17:17 +0800

    Modify model logic for passing api test

--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -1,17 +1,9 @@
 class OrderInfo < ActiveRecord::Base
   include InfoInspector
-  after_update :update_store_data
   
   belongs_to :order
   belongs_to :store, :foreign_key => "ship_store_id"
 
   delegate :address, to: :store
   validates_presence_of :ship_name, :ship_phone, :ship_store_code, :ship_store_id, :ship_store_name, :ship_email
-
-  def update_store_data
-    correct_store = Store.find_by(store_code: self.ship_store_code)
-    unless correct_store.id == self.ship_store_id
-      self.update_columns(ship_store_id: correct_store.id, ship_store_name: correct_store.name)
-    end
-  end
 end
