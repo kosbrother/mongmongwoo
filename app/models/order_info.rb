abaa3843009501cb06e48bb3e3393096633745c0
gvszg <zetaentranceserious@gmail.com>
2016-07-06 18:22:51 +0800

    Modify method naming

--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -1,6 +1,6 @@
 class OrderInfo < ActiveRecord::Base
   include InfoInspector
-  after_update :check_store_id
+  after_update :update_store_data
   
   belongs_to :order
   belongs_to :store, :foreign_key => "ship_store_id"
@@ -8,7 +8,7 @@ class OrderInfo < ActiveRecord::Base
   delegate :address, to: :store
   validates_presence_of :ship_name, :ship_phone, :ship_store_code, :ship_store_id, :ship_store_name, :ship_email
 
-  def check_store_id
+  def update_store_data
     correct_store = Store.find_by(store_code: self.ship_store_code)
     unless correct_store.id == self.ship_store_id
       self.update_columns(ship_store_id: correct_store.id, ship_store_name: correct_store.name)
