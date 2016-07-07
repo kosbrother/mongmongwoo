da548c425d546e4aa1f099b5374b606855a9df5e
gvszg <zetaentranceserious@gmail.com>
2016-07-07 20:29:02 +0800

    Admin can edit stock spec amount

---
+++ app/controllers/admin/stock_specs_controller.rb
@@ -0,0 +1,9 @@
+class Admin::StockSpecsController < AdminController
+  before_action :require_manager
+
+  def update
+    @stock_spec = StockSpec.find(params[:id])
+    @stock_spec.update(params.require(:stock_spec).permit(:amount))
+    respond_to :js
+  end
+end
