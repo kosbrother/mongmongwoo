f71f298b72eaee280867c6979d6410df1150a310
gvszg <zetaentranceserious@gmail.com>
2016-07-07 23:22:40 +0800

    Set jquery selector to class level

--- app/assets/javascripts/admin/order.js
+++ app/assets/javascripts/admin/order.js
@@ -1,14 +1,14 @@
 var get_store;
 get_store = function() {
-  $('#ship_store_code').change(function(){
-    var ship_store_code = $('#ship_store_code').val();
+  $('.ship-store-code').change(function(){
+    var ship_store_code = $('.ship-store-code').val();
     $.ajax({
       url: '/admin/stores/get_store?store_code=' + ship_store_code,
       type: 'GET',
       success: function(data){
         var store = data["data"];
-        $('#ship_store_name').val(store.name);
-        $('#ship_store_id').val(store.id);
+        $('.ship-store-name').val(store.name);
+        $('.ship-store-id').val(store.id);
       },
       error: function(){
         alert('門市資料有誤，請確認門市相關資料');
