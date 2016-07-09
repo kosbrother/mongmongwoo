dbf424d77b95f7e0aa9e00ea232742f3b6497631
gvszg <zetaentranceserious@gmail.com>
2016-07-09 10:15:33 +0800

    Test jquery ui autocomplete

--- app/assets/javascripts/admin/order.js
+++ app/assets/javascripts/admin/order.js
@@ -1,23 +1,42 @@
-var get_store;
+var get_store,
+    availableTags = [
+      "131571",
+      "831598",
+      "962704",
+      "965356",
+      "954035",
+      "138228",
+      "954035",
+      "165415"
+    ];
 get_store = function() {
-  $('.ship-store-code').change(function(){
-    var ship_store_code = $(this).val(),
-        order_id = $(this).data('order-id');
-    $('#submit-' + order_id).attr('disabled', true);
-    $.ajax({
-      url: '/admin/stores/get_store?store_code=' + ship_store_code,
-      type: 'GET',
-      success: function(data){
-        var store = data["data"];
-        $('#store-name-' + order_id).val(store.name);
-        $('#store-id-' + order_id).val(store.id);
-        $('#submit-' + order_id).attr('disabled', false);
-      },
-      error: function(xhr, textStatus, errorThrown){
-        var message = xhr.responseJSON.error.message
-        alert(message);
-      }
-    })
+  $('.btn').click(function(){
+    var order_id = $(this).data('order-id'),
+        ship_store_code = $(this).next().find('#store-code-'+order_id);
+    ship_store_code.change(function(){
+      var store_code = $(this).val(),
+          order_id = $(this).data('order-id');
+      $('#submit-' + order_id).attr('disabled', true);
+      $(this).autocomplete({
+        source: availableTags,
+        appendTo: "#edit_order_"+order_id,
+        change: function( event, ui ) {"option", "appendTo", "#edit_order_"+order_id}
+      });
+      $.ajax({
+        url: '/admin/stores/get_store?store_code=' + store_code,
+        type: 'GET',
+        success: function(data){
+          var store = data["data"];
+          $('#store-name-' + order_id).val(store.name);
+          $('#store-id-' + order_id).val(store.id);
+          $('#submit-' + order_id).attr('disabled', false);
+        },
+        error: function(xhr, textStatus, errorThrown){
+          var message = xhr.responseJSON.error.message
+          alert(message);
+        }
+      })
+    });
   });
 };
 
