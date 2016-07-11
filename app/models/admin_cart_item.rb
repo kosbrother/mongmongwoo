9fe539226fb63cf7ac2bf0938ee141666dc359bb
gvszg <zetaentranceserious@gmail.com>
2016-07-11 21:52:29 +0800

    Refactor admin cart information module

--- app/models/admin_cart_item.rb
+++ app/models/admin_cart_item.rb
@@ -1,5 +1,5 @@
 class AdminCartItem < ActiveRecord::Base
-  include StockInformation
+  include AdminCartInformation
   
   scope :shipping_status, -> { joins(:admin_cart).where(admin_carts: { status: AdminCart::STATUS[:shipping] }) }
   scope :by_cart, -> { includes(:admin_cart) }
@@ -7,9 +7,4 @@ class AdminCartItem < ActiveRecord::Base
   belongs_to :admin_cart
   belongs_to :item
   belongs_to :item_spec
-
-  def shipping_item_quantity
-    items = AdminCartItem.shipping_status.where(item_spec_id: item_spec_id)
-    items.map(&:item_quantity).inject(:+) || 0
-  end
 end
--- app/models/concerns/stock_information.rb
+++
@@ -1,8 +0,0 @@
-module StockInformation
-  extend ActiveSupport::Concern
-
-  def stock_item_quantity
-    stock_items = StockSpec.where(item_spec_id: item_spec_id)
-    stock_items.map(&:amount).inject(:+) || 0
-  end
-end
