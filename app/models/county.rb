2d87261a01f778f77118aada46175e1a2ac4e47d
gvszg <zetaentranceserious@gmail.com>
2016-07-18 15:41:31 +0800

    Modify county and town options to normal method

--- app/models/county.rb
+++ app/models/county.rb
@@ -2,9 +2,12 @@ class County < ActiveRecord::Base
   scope :seven_stores, -> { where("store_type = ?", "4") }
   scope :select_api_fields, -> { select(:id, :name) }
 
-  TAIPEI_ID = 21
-  OPTIONS = County.seven_stores.pluck(:name, :id)
+  TAIPEI_CITY_ID = 21
 
   has_many :towns
   has_many :stores
+
+  def self.options
+    County.seven_stores.pluck(:name, :id)
+  end
 end
--- app/models/town.rb
+++ app/models/town.rb
@@ -1,9 +1,11 @@
 class Town < ActiveRecord::Base
   scope :select_api_fields, -> { select(:id, :name) }
 
-  OPTIONS = Town.where(county_id: County::TAIPEI_ID).pluck(:name, :id)
-  
   belongs_to :county
   has_many :roads
   has_many :stores
+
+  def self.options
+    Town.where(county_id: County::TAIPEI_CITY_ID).pluck(:name, :id)
+  end
 end
