ad6724d0b87f8d8ea99a8eeb69bb8ebb6ca7e27b
gvszg <zetaentranceserious@gmail.com>
2016-07-18 09:05:22 +0800

    Move query method to model

--- app/models/county.rb
+++ app/models/county.rb
@@ -2,6 +2,8 @@ class County < ActiveRecord::Base
   scope :seven_stores, -> { where("store_type = ?", "4") }
   scope :select_api_fields, -> { select(:id, :name) }
 
+  OPTIONS = County.seven_stores.collect{|c| [c.name, c.id]}
+
   has_many :towns
   has_many :stores
 end
--- app/models/town.rb
+++ app/models/town.rb
@@ -1,5 +1,7 @@
 class Town < ActiveRecord::Base
   scope :select_api_fields, -> { select(:id, :name) }
+
+  OPTIONS = County.seven_stores.includes(:towns).first.towns.collect{|t| [t.name, t.id]}
   
   belongs_to :county
   has_many :roads
