5f187e2f7f2db646af63e8f80465f69051aec07f
gvszg <zetaentranceserious@gmail.com>
2016-07-19 17:41:52 +0800

    Refavtor item spec model function sequence

--- app/models/item_spec.rb
+++ app/models/item_spec.rb
@@ -1,17 +1,17 @@
 class ItemSpec < ActiveRecord::Base
   include AdminCartInformation
 
-  scope :recent, -> {order(id: :DESC)}
-  scope :on_shelf, -> {where(status: ItemSpec.statuses[:on_shelf])}
-
   RECOMMEND_STOCK = 5
 
   enum status: { on_shelf: 0, off_shelf: 1 }
 
+  validates_numericality_of :style_amount, :only_integer => true, :greater_than_or_equal_to => 0, :allow_blank => true
+
   belongs_to :item
   has_one :stock_spec
 
-  validates_numericality_of :style_amount, :only_integer => true, :greater_than_or_equal_to => 0, :allow_blank => true
+  scope :recent, -> {order(id: :DESC)}
+  scope :on_shelf, -> {where(status: ItemSpec.statuses[:on_shelf])}
 
   acts_as_paranoid
   mount_uploader :style_pic, SpecPicUploader
