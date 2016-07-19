f29ed4fb4ce32c8876ed77776e242128c70d6356
gvszg <zetaentranceserious@gmail.com>
2016-07-19 16:57:59 +0800

    Setup dummy recommend stock

--- app/models/item_spec.rb
+++ app/models/item_spec.rb
@@ -1,19 +1,19 @@
 class ItemSpec < ActiveRecord::Base
   include AdminCartInformation
-  
+
   scope :recent, -> {order(id: :DESC)}
   scope :on_shelf, -> {where(status: ItemSpec.statuses[:on_shelf])}
 
-  enum status: { on_shelf: 0, off_shelf: 1 }
+  RECOMMEND_STOCK = 5
 
-  acts_as_paranoid
+  enum status: { on_shelf: 0, off_shelf: 1 }
 
   belongs_to :item
   has_one :stock_spec
 
-  # validates_presence_of :style, :style_amount
   validates_numericality_of :style_amount, :only_integer => true, :greater_than_or_equal_to => 0, :allow_blank => true
 
+  acts_as_paranoid
   mount_uploader :style_pic, SpecPicUploader
 
   def spec_id
