705907ab4d153d61140d81947c29970a44d6bdb7
varbark <naoki3355@gmail.com>
2016-07-20 13:54:59 +0800

    rename params search_term to order_search_term

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -77,6 +77,6 @@ class Admin::OrdersController < AdminController
   end
 
   def search_params
-    params.fetch(:search_term, {}).permit(:order_id, :ship_email, :ship_phone)
+    params.require(:order_search_term).permit(:order_id, :ship_email, :ship_phone)
   end
 end
