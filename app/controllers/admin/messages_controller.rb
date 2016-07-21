8c41b646c21c5bc7d3d65c6951bd35f18c77ab24
gvszg <zetaentranceserious@gmail.com>
2016-07-21 17:41:48 +0800

    Separate create office and personal message action

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,5 +1,6 @@
 class Admin::MessagesController < AdminController
   before_action :require_manager
+  before_action :find_user, only: [:my_messages, :my_new_message, :create_my_message]
 
   def index
     @messages = Message.official_messages.paginate(:page => params[:page])
@@ -13,7 +14,7 @@ class Admin::MessagesController < AdminController
     @message = Message.new(message_params)
 
     if @message.save
-      flash[:notice] = "成功新增訊息"
+      flash[:notice] = "成功新增官方訊息"
       redirect_to admin_messages_path
     else
       flash[:danger] = "請檢查訊息內容是否有誤"
@@ -33,18 +34,32 @@ class Admin::MessagesController < AdminController
   end
 
   def my_messages
-    @user = User.find(params[:user_id])
     @messages = @user.messages
   end
 
   def my_new_message
-    @user = User.find(params[:user_id])
     @message = @user.messages.new
+  end
+
+  def create_my_message
+    @message = @user.messages.new(message_params)
+
+    if @message.save
+      flash[:notice] = "成功新增個人訊息"
+      redirect_to my_messages_admin_messages_path(@user)
+    else
+      flash.now[:danger] = "請檢查訊息內容是否有誤"
+      render :my_new_message
+    end
   end
 
   private
 
   def message_params
     params.require(:message).permit(:title, :content, :message_type, user_ids: [])
+  end
+
+  def find_user
+    @user = User.find(params[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -216,6 +216,7 @@ Rails.application.routes.draw do
       collection do
         get "/:user_id/my_messages", to: "messages#my_messages", as: "my_messages"
         get "/:user_id/my_messages/new", to: "messages#my_new_message", as: "my_new_message"
+        post "/:user_id/my_messages", to: "messages#create_my_message"
       end
     end
     resources :promotions, only: [:index, :new, :create, :destroy]
