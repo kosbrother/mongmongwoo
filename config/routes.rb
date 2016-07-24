84fcc8de7a48c5ccaefa061118a552eb968a2767
Steven Ko <chunyuko85@gmail.com>
2016-07-24 14:50:29 +0800

    add status_update path

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,7 @@ Rails.application.routes.draw do
   resources :allpay, only:[] do
     collection do
       post "/create/:order_id", to: "allpay#create", as: "create"
+      post "status_update"
       post "create_reply"
       get "/barcode/:order_id", to: "allpay#barcode", as: "barcode"
     end
