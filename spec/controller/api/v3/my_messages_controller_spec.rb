93db1dc5ee789a333783840b81e27cfdf1797743
gvszg <zetaentranceserious@gmail.com>
2016-07-26 21:42:38 +0800

    Select user before test

--- spec/controller/api/v3/my_messages_controller_spec.rb
+++ spec/controller/api/v3/my_messages_controller_spec.rb
@@ -2,38 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Api::V3::MyMessagesController, type: :controller do
   describe "GET index" do
-    let!(:user) { FactoryGirl.create(:user) }
+    let!(:alice) { FactoryGirl.create(:user) }
     let!(:official_message) { FactoryGirl.create(:official_message) }
-    let!(:personal_message) { FactoryGirl.create(:personal_message, users: [user]) }
+    let!(:personal_message) { FactoryGirl.create(:personal_message, users: [alice]) }
+    let(:user) { User.find(alice.id) }
 
     it "should contain correct message quantity" do
       get :index, user_id: user.id
       data = JSON.parse(response.body)["data"]
-      expect(data.count).to eq(Message.count)
+      expect(data.count).to eq(user.my_messages.count)
     end
 
     it "should contain correct message data" do
       get :index, user_id: user.id
       data = JSON.parse(response.body)["data"]
-      expect(data).to match_array(Message.select_api_fields.as_json)
+      expect(data).to match_array(user.my_messages.select_api_fields.as_json)
     end
   end
 
   describe "GET show" do
-    let!(:user) { FactoryGirl.create(:user) }
+    let!(:alice) { FactoryGirl.create(:user) }
     let!(:official_message) { FactoryGirl.create(:official_message) }
-    let!(:personal_message) { FactoryGirl.create(:personal_message, users: [user]) }
+    let!(:personal_message) { FactoryGirl.create(:personal_message, users: [alice]) }
+    let(:user) { User.find(alice.id) }
 
     it "should contain correct official message data" do
       get :show, user_id: user.id, id: official_message.id
       data = JSON.parse(response.body)["data"]
-      expect(data).to match_array(Message.select_api_fields.find(official_message.id).as_json)
+      expect(data).to match_array(user.my_messages.select_api_fields.find(official_message.id).as_json)
     end
 
     it "should contain correct persernal message data" do
       get :show, user_id: user.id, id: personal_message.id
       data = JSON.parse(response.body)["data"]
-      expect(data).to match_array(Message.select_api_fields.find(personal_message.id).as_json)
+      expect(data).to match_array(user.my_messages.select_api_fields.find(personal_message.id).as_json)
     end
   end
 end
