9db768681ec35bf9556595428cff77607ac5a161
gvszg <zetaentranceserious@gmail.com>
2016-07-26 23:44:28 +0800

    Count items by status

--- app/models/taobao_supplier.rb
+++ app/models/taobao_supplier.rb
@@ -5,6 +5,7 @@ class TaobaoSupplier < ActiveRecord::Base
   delegate :count, :on_shelf, :off_shelf, to: :items
 
   scope :recent, -> { order(id: :DESC) }
+  scope :count_items_by_status, -> { joins(:items).group(:status).count }
 
   def item_ids_in_stock
     items.joins(:stock).map(&:id).join("，")
