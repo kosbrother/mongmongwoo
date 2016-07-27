7f57d6e9887b0c44f01077e99000f88f6dac3d6b
gvszg <zetaentranceserious@gmail.com>
2016-07-27 20:28:31 +0800

    Remove useless view

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,9 +1,7 @@
 class AdminMailer < ApplicationMailer
   def notify_recommend_stock
-    @recommend_stock = ItemSpec.includes(:stock_spec, item: :taobao_supplier, admin_cart_items: :admin_cart).order(item_id: :ASC)
-    @recommend_off_shelf = ItemSpec.includes(:stock_spec, item: :taobao_supplier).recommend_stock_empty
-    @daily_order_quantity = Order.daily_order_quantity
-    @daily_sales_income = OrderItem.daily_sales_income
+    xlsx = render_to_string handlers: [:axlsx], formats: [:xlsx], template: "admin/sales_reports/export_daily_reports"
+    attachments["export_daily_reports.xlsx"] = {mime_type: Mime::XLSX, content: xlsx}
     admin_emails = %w(stevenko@kosbrother.com ping.lin@kosbrother.com jason@kosbrother.com)
     Rails.logger.warn("mail to : #{admin_emails}")
     mail(to: admin_emails, :subject => "[萌萌屋] 推薦庫存不足與推薦下架的樣式清單")
