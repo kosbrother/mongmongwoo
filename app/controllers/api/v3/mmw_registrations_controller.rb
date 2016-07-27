4b2fccaebda09ca5d6ff4d60afee9ebde0bfcbc3
varbark <naoki3355@gmail.com>
2016-07-27 16:08:32 +0800

    api v3 return user_id when login or register

--- app/controllers/api/v3/mmw_registrations_controller.rb
+++ app/controllers/api/v3/mmw_registrations_controller.rb
@@ -2,7 +2,7 @@ class Api::V3::MmwRegistrationsController < ApiController
   def create
     register = User.register(params[:email], params[:password])
     if register[:result]
-      render status: 200, json: {data: "success"}
+      render status: 200, json: {data: {user_id: register[:user].id}}
     else
       message = register[:message].join(' ')
       render status: 400, json: {error: {message: message}}
@@ -14,7 +14,7 @@ class Api::V3::MmwRegistrationsController < ApiController
     if user.nil? || user.is_mmw_registered == false
       render status: 400, json: {error: {message: t('controller.error.message.no_user')}}
     elsif  user.authenticate(params[:password])
-      render status: 200, json: {data: "success"}
+      render status: 200, json: {data: {user_id: user.id}}
     else
       render status: 400, json: {error: {message: t('controller.error.message.wrong_password')}}
     end
