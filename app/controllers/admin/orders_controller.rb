258c9fe6d4e52a4b42b592c273dc99cdc65b643e
varbark <naoki3355@gmail.com>
2016-08-03 09:34:45 +0800

    rename routes to update_to_processing

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -77,7 +77,7 @@ class Admin::OrdersController < AdminController
     redirect_to status_index_admin_orders_path(status: Order.statuses[order.status], anchor: "order-id-#{order.id}")
   end
 
-  def update_to_placed
+  def update_to_processing
     new_orders = Order.status(Order.statuses["新訂單"])
     new_orders.each do |order|
       order.update_attribute(:status, Order.statuses["處理中"]) if order.all_able_to_pack?
