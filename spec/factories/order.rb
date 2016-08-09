219f4df5601967c6422fcbaf94440f8d6ecc00a0
gvszg <zetaentranceserious@gmail.com>
2016-08-09 16:35:37 +0800

    Remove unused spec order setting

--- spec/factories/order.rb
+++ spec/factories/order.rb
@@ -6,15 +6,5 @@ FactoryGirl.define do
     total Faker::Commerce.price
     ship_fee Faker::Commerce.price
     association :info, factory: :order_info
-    factory :order_with_items do
-      transient do
-        items_count 3
-      end
-
-      after(:create) do |order, evaluator|
-        FactoryGirl.create_list(:order_item, evaluator.items_count, :order => order)
-      end
-    end
   end
-
 end
