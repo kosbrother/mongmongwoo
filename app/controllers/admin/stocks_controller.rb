2fec377df2b740887b97bc0c4f107c006593c33a
gvszg <zetaentranceserious@gmail.com>
2016-08-12 15:11:59 +0800

    Modify stock view

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -3,6 +3,9 @@ class Admin::StocksController < AdminController
 
   def index
     @taobao_suppliers = TaobaoSupplier.recent
+    params[:taobao_supplier_id] ||= @taobao_suppliers.last.id
+    @taobao_supplier = @taobao_suppliers.find(params[:taobao_supplier_id])
+    @items = @taobao_supplier.items.includes(stock_specs: :item_spec).paginate(page: params[:page])
   end
 
   def stock_lists
