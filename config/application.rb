854328f8dbeb1fda2ddf207fb84ca4cd61d4cf06
Steven Ko <chunyuko85@gmail.com>
2016-08-13 10:36:12 +0800

    fix model delay method not find error

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'csv'
-require 'sidekiq/api'
 require 'rails/all'
+require 'sidekiq/api'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
