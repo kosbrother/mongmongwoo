4030f59f29d402c8b9e4aa07faefb61fe5fdac4d
Steven Ko <chunyuko85@gmail.com>
2016-08-16 15:27:36 +0800

    fix order_items_helper error

--- app/helpers/admin/order_items_helper.rb
+++ app/helpers/admin/order_items_helper.rb
@@ -3,7 +3,7 @@ module Admin::OrderItemsHelper
     statuses = []
     statuses << '商品已下架' unless order_item.item_on_shelf?
     statuses << '樣式已下架' unless order_item.item_spec_on_shelf?
-    statuses << '庫存不足' unless order_item.stock_amount_enough?
+    statuses << '庫存不足' unless (order_item.item_spec.stock_item_quantity - order_item.item_spec.requested_quantity >= 0)
     statuses.join('，')
   end
 
