c5f9c5fe174180c16545f39eebe0d1e77ca252d7
Steven Ko <chunyuko85@gmail.com>
2016-08-19 22:26:10 +0800

    modify api items per_page

--- app/controllers/api/v3/items_controller.rb
+++ app/controllers/api/v3/items_controller.rb
@@ -2,9 +2,9 @@ class Api::V3::ItemsController < ApiController
   def index
     category = Category.find(params[:category_id])
     if(params[:sort].present?)
-      items = category.items.on_shelf.order(Item.sort_params[params[:sort]]).includes(:on_shelf_specs).select(:id, :name, :price, :special_price, :cover, :slug).page(params[:page]).per_page(20)
+      items = category.items.on_shelf.order(Item.sort_params[params[:sort]]).includes(:on_shelf_specs).select(:id, :name, :price, :special_price, :cover, :slug).page(params[:page]).per_page(30)
     else
-      items = category.items.on_shelf.priority.includes(:on_shelf_specs).select(:id, :name, :price, :special_price, :cover, :slug).page(params[:page]).per_page(20)
+      items = category.items.on_shelf.priority.includes(:on_shelf_specs).select(:id, :name, :price, :special_price, :cover, :slug).page(params[:page]).per_page(30)
     end
 
     datas = items.as_json(include: { on_shelf_specs: { only: [:id, :style, :style_pic], methods: [:stock_amount] } })
