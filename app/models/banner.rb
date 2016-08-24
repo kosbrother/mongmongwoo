b543701704ce7fc8cccd2bbd5a818e358c53fe9e
gvszg <zetaentranceserious@gmail.com>
2016-08-24 18:03:11 +0800

    Modify: use module to return app_index_url

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,4 +1,6 @@
 class Banner < ActiveRecord::Base
+  include AndroidApp
+
   CATEGORY_RECORD = "Category"
   Item_RECORD = "Item"
   RECORD_TYPES = [CATEGORY_RECORD, Item_RECORD]
@@ -9,4 +11,16 @@ class Banner < ActiveRecord::Base
   scope :recent, -> { order(id: :DESC) }
 
   mount_uploader :image, OriginalPicUploader
+
+  def able_path
+    case bannerable_type
+    when CATEGORY_RECORD
+      category = bannerable
+      category_path(category)
+    when Item_RECORD
+      item = bannerable
+      category = item.categories.last
+      category_item_path(category, item)
+    end
+  end
 end
