707db7e583a1497d8fd1269585c38813d89fa158
gvszg <zetaentranceserious@gmail.com>
2016-08-25 00:58:35 +0800

    Modify: as_json select attributes query

--- app/controllers/api/v4/banners_controller.rb
+++ app/controllers/api/v4/banners_controller.rb
@@ -1,6 +1,6 @@
 class Api::V4::BannersController < ApiController
   def index
-    banners = Banner.recent.as_json(except: [:created_at, :updated_at], methods: :app_index_url)
+    banners = Banner.recent.as_json(only: [:id, :bannerable_id, :bannerable_type, :title, :image], methods: :app_index_url)
 
     render status: 200, json: { data: banners }
   end
