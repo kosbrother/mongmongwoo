9823f77f103b3d0795c25216000efdea36668f16
Steven Ko <chunyuko85@gmail.com>
2016-08-25 18:40:18 +0800

    log params when registration_id not provide

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,9 @@
 class Api::SessionsController < ApiController
   def reguire_registration_id
-    render status: 400, json: t('controller.error.message.no_regitration_id') if params[:registration_id].blank?
+    if params[:registration_id].blank?
+      Rails.logger.info(t('controller.error.message.no_regitration_id'))
+      Rails.logger.info(params.inspect)
+      render status: 400, json: t('controller.error.message.no_regitration_id')
+    end
   end
 end
