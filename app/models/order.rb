1a3c6b133169a4795ee6ca21d1171f62e283dacc
gvszg <zetaentranceserious@gmail.com>
2016-08-30 21:16:05 +0800

    Refactor: move logic to model

--- app/models/order.rb
+++ app/models/order.rb
@@ -46,6 +46,20 @@ class Order < ActiveRecord::Base
 
   self.per_page = 20
 
+  def self.generate_home_delivery_order_xls(orders)
+    workbook = Spreadsheet::Workbook.new
+    sheet = workbook.create_worksheet name: "宅配訂單清單"
+
+    orders.each_with_index do |order, index|
+      row = sheet.row(index)
+      row.push order.ship_name, order.ship_phone, order.zip_code, order.ship_address, "4277907101", "", order.id, "", "1", "文具飾品", order.total, "1", order.note, "", ""
+    end
+
+    spreadsheet = StringIO.new
+    workbook.write spreadsheet
+    spreadsheet.string
+  end
+
   def survey_mail
     mail_records.find_by(mail_type: MailRecord.mail_types["satisfaction_survey"])
   end
