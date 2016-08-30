157f68ab3da216d5f85d4b2a4d280fd14be92ee1
Steven Ko <chunyuko85@gmail.com>
2016-08-30 16:09:53 +0800

    modify index cateogry items query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :load_popular_items, :load_categories
 
   def index
-    @category_with_items = @categories.map { |category| {category: category, items: category.items.on_shelf.latest(6)} }
+    @category_with_items = @categories.map { |category| {category: category, items: category.items.on_shelf.order(Item.sort_params["popular"]).latest(9)} }
     set_meta_tags title: '首頁'
   end
 
