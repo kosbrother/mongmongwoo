74b8efd26d711527e910e488abb8d752b6f6328e
varbark <naoki3355@gmail.com>
2016-09-02 12:09:26 +0800

    rename web items#show spec to item_spec

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,8 @@ class ItemsController < ApplicationController
   def show
     @category = Category.find(params['category_id'])
     @item = Item.find(params['id'])
-    @item.specs.on_shelf.size == 0 ? @specs = @item.specs : @specs = @item.specs.on_shelf
-    @first_item_spec = @specs.detect{|spec| spec.stock_amount > 0}
+    @item.specs.on_shelf.size == 0 ? @item_specs = @item.specs : @item_specs = @item.specs.on_shelf
+    @first_item_spec = @item_specs.detect{|spec| spec.stock_amount > 0}
     @first_item_spec = @item_specs.first if @first_item_spec.blank?
 
     set_meta_tags title: @item.name,
