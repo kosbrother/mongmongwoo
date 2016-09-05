5d8d411f85eb4fd365c457a4a500d05ad9539eb7
varbark <naoki3355@gmail.com>
2016-09-05 10:58:22 +0800

    category shoe page add sibling categories carousel

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,9 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params['id'])
     @items = @category.items.on_shelf.priority.paginate(page: params['page'], per_page: 18)
+    parent_id = @category.parent_id || @category.id
+    @child_categories = Category.where(parent_id: parent_id)
+
     set_meta_tags title: @category.name,
                   keywords: @category.name
   end
