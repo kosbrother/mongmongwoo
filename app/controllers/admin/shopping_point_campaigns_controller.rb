a68d2189e00f78643391ea3b0a2466044b78a899
varbark <naoki3355@gmail.com>
2016-09-07 09:29:20 +0800

    admin shopping point campaigns show

--- app/controllers/admin/shopping_point_campaigns_controller.rb
+++ app/controllers/admin/shopping_point_campaigns_controller.rb
@@ -5,4 +5,8 @@ class Admin::ShoppingPointCampaignsController < AdminController
     @is_expired = params[:is_expired] == 'true'
     @shopping_point_campaigns = ShoppingPointCampaign.where(is_expired: @is_expired).order(id: :desc).paginate(:page => params[:page])
   end
+
+  def show
+    @shopping_point_campaign = ShoppingPointCampaign.find(params[:id])
+  end
 end
