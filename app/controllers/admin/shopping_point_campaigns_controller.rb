d6559050398ca72522253aa70c02bd977106f880
varbark <naoki3355@gmail.com>
2016-09-07 14:37:03 +0800

    admin shopping point campaigns edit

--- app/controllers/admin/shopping_point_campaigns_controller.rb
+++ app/controllers/admin/shopping_point_campaigns_controller.rb
@@ -1,13 +1,10 @@
 class Admin::ShoppingPointCampaignsController < AdminController
   before_action :require_manager
+  before_action :find_shopping_point_campaign, only: [:show, :edit, :update, :expired]
 
   def index
     @is_expired = params[:is_expired] == 'true'
     @shopping_point_campaigns = ShoppingPointCampaign.where(is_expired: @is_expired).order(id: :desc).paginate(:page => params[:page])
-  end
-
-  def show
-    @shopping_point_campaign = ShoppingPointCampaign.find(params[:id])
   end
 
   def create
@@ -22,9 +19,23 @@ class Admin::ShoppingPointCampaignsController < AdminController
     end
   end
 
+  def update
+    if @shopping_point_campaign.update(shopping_point_campaign_params)
+      flash[:notice] = "成功更新購物金活動"
+      redirect_to admin_shopping_point_campaigns_path
+    else
+      flash[:danger] = "請檢查更新內容是否有誤"
+      redirect_to :edit
+    end
+  end
+
   private
 
   def shopping_point_campaign_params
     params.require(:shopping_point_campaign).permit(:title, :description, :amount, :created_at, :valid_until, :is_reusable)
+  end
+
+  def find_shopping_point_campaign
+    @shopping_point_campaign = ShoppingPointCampaign.find(params[:id])
   end
 end
