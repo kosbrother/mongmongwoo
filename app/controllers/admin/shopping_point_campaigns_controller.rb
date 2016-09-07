67414d78daf71648c20b05b28a2245afb5c871b4
varbark <naoki3355@gmail.com>
2016-09-07 13:06:30 +0800

    admin shopping point campaigns create

--- app/controllers/admin/shopping_point_campaigns_controller.rb
+++ app/controllers/admin/shopping_point_campaigns_controller.rb
@@ -9,4 +9,22 @@ class Admin::ShoppingPointCampaignsController < AdminController
   def show
     @shopping_point_campaign = ShoppingPointCampaign.find(params[:id])
   end
+
+  def create
+    shopping_point_campaign = ShoppingPointCampaign.new(shopping_point_campaign_params)
+
+    if shopping_point_campaign.save
+      flash[:notice] = "成功新增購物金活動"
+      redirect_to admin_shopping_point_campaigns_path
+    else
+      flash[:danger] = "請檢查活動內容是否有誤"
+      render :new
+    end
+  end
+
+  private
+
+  def shopping_point_campaign_params
+    params.require(:shopping_point_campaign).permit(:title, :description, :amount, :created_at, :valid_until, :is_reusable)
+  end
 end
