a7b1af4929e686eeab9afea8c7aeb53a1c8d4d27
gvszg <zetaentranceserious@gmail.com>
2016-09-07 09:31:42 +0800

    Modify: extract notify wish list message same logic to another method

--- app/services/user_notify_service.rb
+++ app/services/user_notify_service.rb
@@ -8,22 +8,12 @@ class UserNotifyService
 
   def wish_list_item_spec_on_shelf(item_spec)
     message = user.messages.create(item_spec_on_shelf_params(item_spec.item, item_spec.style))
-
-    if user.device_id
-      registration_id = [DeviceRegistration.find(user.device_id).registration_id]
-      options = GcmOptionsGenerator.generate_options_for_send_message(message)
-      GcmNotifyService.new.send_message_notification(registration_id, options)
-    end
+    send_wish_list_message_to_user(message) if user.device_id
   end
 
   def wish_list_item_spec_arrival(item_spec)
     message = user.messages.create(item_spec_arrival_params(item_spec.item, item_spec.style))
-
-    if user.device_id
-      registration_id = [DeviceRegistration.find(user.device_id).registration_id]
-      options = GcmOptionsGenerator.generate_options_for_send_message(message)
-      GcmNotifyService.new.send_message_notification(registration_id, options)
-    end
+    send_wish_list_message_to_user(message) if user.device_id
   end
 
   def notify_to_pick_up(order)
@@ -56,5 +46,11 @@ class UserNotifyService
     {title: "萌萌屋取貨通知",
      content: "#{order.ship_name} 您好，您訂購的商品已送達#{order.ship_store_name}門市，請於七日內完成取貨。若有任何問題，請聯繫萌萌屋客服。",
      message_type: Message.message_types["個人訊息"]}
+  end
+
+  def send_wish_list_message_to_user(message)
+    registration_id = [DeviceRegistration.find(user.device_id).registration_id]
+    options = GcmOptionsGenerator.generate_options_for_send_message(message)
+    GcmNotifyService.new.send_message_notification(registration_id, options)
   end
 end
