4d3e83d1fb1c9ab290b4dad2be770c187defb458
Steven Ko <chunyuko85@gmail.com>
2016-09-09 22:22:57 +0800

    refactor code

--- app/services/gcm_notify_service.rb
+++ app/services/gcm_notify_service.rb
@@ -8,12 +8,8 @@ class GcmNotifyService
   end
 
   def send_notification(registration_ids, options)
-    if registration_ids.size > 1000
-      registration_ids.each_slice(1000) do |per_registration_ids|
-        @gcm.send_notification(per_registration_ids, options)
-      end
-    else
-      @gcm.send_notification(registration_ids, options)
+    registration_ids.each_slice(1000) do |per_registration_ids|
+      @gcm.send_notification(per_registration_ids, options)
     end
   end
 end
