7f8862d03b3a5362e3607f2f82917a13696e057a
Steven Ko <chunyuko85@gmail.com>
2016-09-14 15:01:39 +0800

    fix annoymous has no order message error

--- app/services/user_notify_service.rb
+++ app/services/user_notify_service.rb
@@ -49,7 +49,11 @@ class UserNotifyService
   end
 
   def notify_to_pick_up(order)
-    message = user.messages.create(order_message_params(order)) if !(user.anonymous_user?)
+    if user.anonymous_user?
+      message = Message.create(order_message_params(order))
+    else
+      message = user.messages.create(order_message_params(order))
+    end
 
     if order.device_registration
       registration_ids = [order.device_registration.registration_id]
