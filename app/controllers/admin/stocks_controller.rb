5d43ec724c8fb68e1222e9be24f268680c7faebc
Steven Ko <chunyuko85@gmail.com>
2016-09-23 09:38:30 +0800

    stock index page romove supplier filter & add status update button

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -2,9 +2,7 @@ class Admin::StocksController < AdminController
   before_action :require_manager
 
   def index
-    @taobao_suppliers = TaobaoSupplier.all
-    params[:taobao_supplier_id] ||= @taobao_suppliers.first.id
-    query_hash = {taobao_supplier_id: params[:taobao_supplier_id]}
+    query_hash = {}
     query_hash = query_hash.merge(status: params[:status], ever_on_shelf: true) if params[:status]
     query_hash = query_hash.merge(ever_on_shelf: false) if params[:ever_on_shelf] == 'false'
     @items = Item.includes(:specs).where(query_hash).recent.paginate(page: params[:page])
