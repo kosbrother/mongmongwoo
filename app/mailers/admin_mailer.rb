7c7b4bfc30ad14fc554d107c30f03c2a045d4af9
Steven Ko <chunyuko85@gmail.com>
2016-10-01 08:17:15 +0800

    modify daily report mail list & file name

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -12,8 +12,8 @@ class AdminMailer < ApplicationMailer
     cancel_quantity_and_income = orders.where(status: Order.statuses["訂單取消"]).count_and_income_fields[0]
     cancel_quantity, cancel_income = cancel_quantity_and_income["quantity"], cancel_quantity_and_income["income"]
     xlsx = render_to_string handlers: [:axlsx], formats: [:xlsx], template: "admin/sales_reports/export_daily_reports", locals: { recommend_stock: recommend_stock, recommend_off_shelf: recommend_off_shelf, total_quantity: total_quantity, total_income: total_income, repurchased_quantity: repurchased_quantity, repurchased_income: repurchased_income, cancel_quantity: cancel_quantity, cancel_income: cancel_income }, layout: false
-    attachments["export_daily_reports.xlsx"] = {mime_type: Mime::XLSX, content: xlsx}
-    admin_emails = %w(stevenko@kosbrother.com ping.lin@kosbrother.com jason@kosbrother.com)
+    attachments["#{Date.yesterday.strftime("%Y_%m_%d")}_report.xlsx"] = {mime_type: Mime::XLSX, content: xlsx}
+    admin_emails = %w(stevenko@kosbrother.com ping.lin@kosbrother.com jason@kosbrother.com beatles1030@gmail.com)
     Rails.logger.warn("mail to : #{admin_emails}")
     mail(to: admin_emails, :subject => "[萌萌屋] 推薦庫存不足與推薦下架的樣式清單") {|format| format.text {render plain: "附件內容：建議庫存，下架清單與昨日銷售"}}
   end
