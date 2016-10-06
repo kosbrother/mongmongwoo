e5e7ad77bf896fb85159c579cf870990802fd1c5
Steven Ko <chunyuko85@gmail.com>
2016-10-06 18:00:52 +0800

    fix admin/shoppint_poiints_controller role bug

--- app/controllers/admin/shopping_points_controller.rb
+++ app/controllers/admin/shopping_points_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ShoppingPointsController < AdminController
-  before_action :require_manager
+  before_action do
+    accept_role(:manager)
+  end
   before_action :find_user
 
   def index
