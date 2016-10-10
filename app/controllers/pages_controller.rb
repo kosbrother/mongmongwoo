84970d67f4774aa33e9c80f643bebbb86d55d75a
Steven Ko <chunyuko85@gmail.com>
2016-10-10 11:25:56 +0800

    change web index and product show meta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,14 @@ class PagesController < ApplicationController
     @banners = Banner.order(id: :desc)
     @category_with_items = @categories.map { |category| {category: category, items: category.items.on_shelf.priority.latest(12)} }
     set_meta_tags title: '首頁',
+                  description: '萌萌屋是學生網購天堂，筆袋、耳機、禮品、飾品、水瓶、餐具、包包、雜貨、日韓流行、保養、貼紙、紙膠帶、文具、等校園生活萌物，APP購物現貨不必等。萌萌屋提供年輕女生可愛新奇又特別的小東西，周周新品活動促銷。療癒搞怪都在萌萌屋。',
+                  keywords: Category.except_the_all_category.select("name").map(&:name),
                   og: {
-                    title:       "青年流行最前線",
+                    title:       "萌萌屋 | 校園生活補給站",
                     type:        "website",
                     url:         root_url,
                     image:       view_context.asset_url(@banners.first.image_url),
-                    description: "萌萌屋走在青年流行的前線, 為年輕人們提供一個快樂購物的管道, 想知道最近流行些什麼, 有什麼可愛的東東, 來萌萌屋就對了!"
+                    description: "萌萌屋是學生網購天堂，筆袋、耳機、禮品、飾品、水瓶、餐具、包包、雜貨、日韓流行、保養、貼紙、紙膠帶、文具、等校園生活萌物，APP購物現貨不必等。萌萌屋提供年輕女生可愛新奇又特別的小東西，周周新品活動促銷。療癒搞怪都在萌萌屋。"
                   }
   end
 
