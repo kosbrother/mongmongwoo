e77e832f2a94cc7838215f6144893b9f81872111
Steven Ko <chunyuko85@gmail.com>
2016-10-10 12:31:51 +0800

    category meta

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,15 +8,25 @@ class CategoriesController < ApplicationController
     @sort_options = Item.sort_params.keys.map {|key| [t("models.item.sort_params.#{key}"), key]}
     @items_sort = params[:items_sort] || @sort_options.first[1]
     @items = @category.items.on_shelf.order(Item.sort_params[@items_sort]).paginate(page: params['page'], per_page: 18)
+    if @category.parent_id
+      siblings_name = @child_categories.reject{|c| c.id == @category.id}.map(&:name).join(",")
+      meta_description = "「#{@category.name}」類別商品, 周周上新品, 校園生活萌物，APP購物現貨不必等, 萌萌屋是學生的網購天堂。另有相關類別：#{siblings_name}。"
+      meta_keywords = @child_categories.map(&:name)
+    else
+      childs_name = @child_categories.map(&:name).join(",")
+      meta_description = "萌萌屋「#{@category.name}」類別商品, 周周上新品, 校園生活萌物，APP購物現貨不必等, 萌萌屋是學生的網購天堂。另有子類別：#{childs_name}。"
+      meta_keywords = @child_categories.map(&:name).push(@category.name)
+    end
 
     set_meta_tags title: @category.name,
-                  keywords: @category.name,
+                  description: meta_description,
+                  keywords: meta_keywords,
                   og: {
-                    title:       @category.name,
+                    title:       "#{@category.name}-萌萌屋(校園生活補給站)",
                     type:        "product.group",
                     url:         category_url(@category),
                     image:       view_context.asset_url(@category.image_url),
-                    description: @category.name
+                    description: meta_description
                   }
   end
 end
