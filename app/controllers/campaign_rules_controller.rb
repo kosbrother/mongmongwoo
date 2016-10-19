1056e41cc7fd40b1d163dfc259736c8bc9ebeb18
varbark <naoki3355@gmail.com>
2016-10-19 11:16:57 +0800

    web campaign show page

--- app/controllers/campaign_rules_controller.rb
+++ app/controllers/campaign_rules_controller.rb
@@ -4,6 +4,13 @@ class CampaignRulesController < ApplicationController
 
   def index
     @campaign_rules = CampaignRule.valid
+  end
 
+  def show
+    @campaign_rule = CampaignRule.find(params[:id])
+    @discount_icon_url = @campaign_rule.card_icon.url
+    @items = @campaign_rule.items
+    @category = Category.find(Category::NEW_ID)
+    render :layout => 'campaign'
   end
 end
