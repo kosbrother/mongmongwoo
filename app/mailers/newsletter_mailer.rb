0815199a4e28e89e4abf9d2c6eb54e761502efbf
gvszg <zetaentranceserious@gmail.com>
2016-10-26 10:11:59 +0800

    Modify mailer controller query

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -5,9 +5,9 @@ class NewsletterMailer < ApplicationMailer
     @title = "活動電子報"
     @categories = Category.parent_categories
     @the_all_category = Category.find(Category::ALL_ID)
-    @popular_items = Item.on_shelf.joins(:item_categories).where('item_categories.category_id = :category_id', category_id: Category::ALL_ID).priority.limit(6)
+    @popular_items = @the_all_category.items.on_shelf.priority.limit(6)
     @the_new_category = Category.find(Category::NEW_ID)
-    @newest_items = Item.on_shelf.joins(:item_categories).where('item_categories.category_id = :category_id', category_id: Category::NEW_ID).order(created_at: :desc).limit(6)
+    @newest_items = @the_new_category.items.on_shelf.order(created_at: :desc).limit(6)
     User.where.not(email: nil).find_in_batches do |user|
       mail(to: user.email, subject: "[萌萌屋] 萌萌屋#{@title}")
     end
